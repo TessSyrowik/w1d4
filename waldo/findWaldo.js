4a72252646bf3da24a7c2192f5c30540afd349bd
Tess Syrowik <tess.syrowik@gmail.com>
2018-03-15 18:47:00 +0000

    Updated Waldo with forEach

--- waldo/findWaldo.js
+++ waldo/findWaldo.js
@@ -1,9 +1,33 @@
+// Exercise #1: Callback Arguments:
+
+
+
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(arr[i], i);   // execute callback
+//     }
+//   }
+// }
+
+
+
+// function actionWhenFound(arrayElement, elementIndex) {
+//   console.log("Found " + arrayElement + " at index " + elementIndex + "!");
+// }
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+//Exercise #2: Array forEach
+
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(arr[i], i);   // execute callback
+  arr.forEach(function(arrayElement) {
+    if (arrayElement === "Waldo") {
+      found(arrayElement, arr.indexOf(arrayElement))   // execute callback
     }
-  }
+  });
 }
 
 
@@ -13,7 +37,5 @@ function actionWhenFound(arrayElement, elementIndex) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
-
 
 
